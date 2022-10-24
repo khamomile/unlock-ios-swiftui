ad7e5b066d3c2c4444b0f3a3a836beb5f05f6112
csehuman <paullee20204@snu.ac.kr>
2022-10-24 10:43:12 +0900

    Initial Commit

---
+++ unlock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  unlock
+//
+//  Created by Paul Lee on 2022/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ unlock/unlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  unlockApp.swift
+//  unlock
+//
+//  Created by Paul Lee on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct unlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
